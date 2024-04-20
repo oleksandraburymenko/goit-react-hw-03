72742a2f285e250d25906e4b9299c2dd6691abd0
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2024-04-20 21:48:31 +0300

    add code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from './Phonebook.module.css';
+import { ContactForm } from './ContactForm';
+import {Section} from './Section';
+import { Component } from 'react';
+import { Filter } from './Filter';
+import { ContactList } from './ContactList';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+export class App extends Component {
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
+  };
+
+
+componentDidMount() {
+  const contacts = localStorage.getItem('contacts')
+  if (contacts) {
+      this.setState({contacts:JSON.parse(contacts)})
+  }
+}
+componentDidUpdate(prevProps,prevState) {
+  if (this.state.contacts !== prevState.contacts) {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+  }
 }
 
-export default App
+
+  filterContact = event => {
+    this.setState ({filter: event.target.value});
+  };
+
+  formSubmitHandler = ({ name, number }) => {
+    const newContact = {
+      id: shortid.generate(),
+      name,
+      number
+    };
+    
+    if (this.state.contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+      alert(`${name} is already in contacts`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
+  }
+
+  deleteContact = id => {
+    this.setState(prevContact => ({
+      contacts: prevContact.contacts.filter(contact => contact.id !== id)
+    }))
+  }
+  
+  normalizedContact = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+      );
+  }
+
+  render () {
+    const { filter } = this.state;
+    return (
+      <div className={css.container}>
+        <>
+        <Section title="Phonebook">
+          <ContactForm onSubmit = { this.formSubmitHandler }/>
+        </Section>
+        <Section title="Contacts">
+          <Filter
+          filter={filter}
+          filterContact={this.filterContact}
+          />
+          <ContactList
+          contacts={this.normalizedContact()}
+          deleteContact={this.deleteContact}
+          />
+        </Section>
+        </>
+      </div>
+    );
+  }
+
+};
